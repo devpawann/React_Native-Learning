1f70fd2307be82af61532ee5139dad3c4104acb8
Pawan Acharya <pawanacharya070@gmail.com>
2021-07-09 17:21:08 +0545

    volatile todo list

--- first_app/App.js
+++ first_app/App.js
@@ -1,21 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from "expo-status-bar";
+import React, {useState} from "react";
+import {
+  Button,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [totalTask, settotalTask] = useState([]);
+  const [task, settask] = useState("");
+
+  function handleTextField(task) {
+    settask(task);
+  }
+  function btnPressHandle() {
+    settotalTask([...totalTask, task]);
+    console.log(totalTask);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={style.main}>
+      <TextInput
+        placeholder="Hello World"
+        style={style.inputField}
+        value={task}
+        onChangeText={handleTextField}
+      />
+      <Button title="Add" style={style.btn} onPress={btnPressHandle} />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const style = StyleSheet.create({
+  main: {
+    padding: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  inputField: {
+    padding: 14,
+    borderWidth: 2,
+    borderColor: "blue",
+    marginRight: 20,
+    //width: "80%",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  btn: {
+    padding: 20,
+    flex: 1,
   },
 });
